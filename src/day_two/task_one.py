d105bad65a7dab07145b9ed526de03f386ed5686
Chris Rossell <chris.rossell@autotrader.co.uk>
2024-12-05 09:17:53 +0000

    Day 2 task 1 solution

---
+++ src/day_two/task_one.py
@@ -0,0 +1,84 @@
+import pandas
+
+def main():
+    report_values = get_reports_from_input()
+    no_safe_reports = get_number_of_safe_reports(report_values)
+    print(no_safe_reports)
+
+
+def get_reports_from_input():
+
+    report_dataframe = pandas.read_csv('input.txt', sep=' ', engine='python')
+
+    list_of_reports = report_dataframe.values.tolist()
+    print(list_of_reports)
+
+    return list_of_reports
+
+
+def get_number_of_safe_reports(list_of_reports):
+    no_safe_reports = 0
+    for current_report in list_of_reports:
+        cleaned_report = [x for x in current_report if str(x) != 'nan']
+
+        if is_safe_report(cleaned_report):
+            no_safe_reports += 1
+
+    return no_safe_reports
+
+
+
+def is_static_increment(difference):
+    if difference == 0:
+        return True
+    else:
+        return False
+
+
+def is_increasing_increment(difference):
+    if difference > 0:
+        return True
+    else:
+        return False
+
+def is_valid_increment(difference):
+    if 3 >= difference > 0:
+        return True
+    else:
+        return False
+
+
+
+def is_safe_report(report):
+    last_index_in_report = len(report) - 1
+
+    # get if the list is increasing or decreasing to compare each increment against
+    first_diff = report[1] - report[0]
+    if is_static_increment(first_diff):
+        return False
+    is_increasing_report = is_increasing_increment(first_diff)
+
+
+    # iterate through the list, checking the magnitude of the difference and the direction
+    for index, level in enumerate(report):
+        print("value: ", level)
+        print("index: ", index)
+
+
+        if index < last_index_in_report:
+            next_level = report[index + 1]
+            difference = (next_level - level)
+
+            if is_static_increment(difference):
+                return False
+            is_current_increment_increasing = is_increasing_increment(difference)
+            if is_current_increment_increasing != is_increasing_report:
+                return False
+
+            if not is_valid_increment(abs(difference)):
+                return False
+    return True
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/day_two/test_task_one.py
@@ -0,0 +1,12 @@
+from day_two.task_one import is_safe_report
+
+
+def test_is_safe_report():
+    report = [7, 6, 4, 2, 1]
+    assert is_safe_report(report) == True
+#
+# 1 2 7 8 9
+# 9 7 6 2 1
+# 1 3 2 4 5
+# 8 6 4 4 1
+# 1 3 6 7 9
