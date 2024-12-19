5f4f25cf1606b1c217fb66da896543795100e335
Chris Rossell <chris.rossell@autotrader.co.uk>
2024-12-19 17:05:06 +0000

    add tests to show missing logic

--- test/day_two/test_task_two.py
+++ test/day_two/test_task_two.py
@@ -1,6 +1,16 @@
 from day_two.task_two import is_not_static_and_is_correct_magnitude_increment
 from src.day_two.task_two import get_number_of_safe_reports_with_dampener, is_safe_report_with_dampener
 
+
+def test_safe_report_when_increasing():
+    report = [1, 3, 6, 7, 9]
+    expected = True
+
+    actual = is_safe_report_with_dampener(report)
+
+    assert actual == expected
+
+
 def test_safe_report_when_decreasing():
     report = [7, 6, 4, 2, 1]
     expected = True
@@ -57,18 +67,40 @@ def test_safe_report_when_decreasing_and_need_to_remove():
     assert actual == expected
 
 
-def test_safe_report_when_increasing():
-    report = [1, 3, 6, 7, 9]
+
+# I think my logic is flawed --> at the time of recognising an increment which is not valid, it could be either the
+# current value or the next value that is the problem value
+# and I need to try and remove both of them
+# write a test case to show this, where just removign the first element results i nan incrorect answer
+
+# logically there wsa a single case where a report could fail;
+# increment is not of correct magnitude + direction
+
+# because we can now skip a value, there is now two extra cases
+# increment is not of correct magnitude + direction and removing current value (increment[i-1:i+1]) is valid c--> continue with updated report (recursion --> it can  call the function from the first task which has no tolerance for removing values --> if it calls itself another problem value could be skipped similiarly)
+# increment is not of correct magnitude + direction and removing next value (increment[i:i+2]) is valid --> continue with updated report
+# increment is not of correct magnitude + direction and removing next value or current value is not valid --> fail
+
+
+def test_safe_report_where_increasing_and_second_value_is_problem():
+    # if the last 1 is removed then the report is valid
+    report = [1, 2, 1, 4, 5]
     expected = True
 
     actual = is_safe_report_with_dampener(report)
 
     assert actual == expected
 
-# I think my logic is flawed --> at the time of recognising an increment which is not valid, it could be either the
-# current value or the next value that is the problem value
-# and I need to try and remove both of them
-# logically if there is an increment which fails due to sign, the value after could
+def test_safe_report_where_decreasing_and_second_value_is_problem():
+    report = [8, 6, 5, 1, 4]
+    expected = True
+
+    actual = is_safe_report_with_dampener(report)
+
+    assert actual == expected
+
+
+# valid increment tests
 
 def test_is_valid_increment_positive():
     difference = 3
