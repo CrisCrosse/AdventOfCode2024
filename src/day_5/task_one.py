bf97d9bf48a461dab1f0ca29e7e6b5f560c67752
Chris Rossell <chris.rossell@autotrader.co.uk>
2025-01-03 11:24:40 +0000

    day 5 task one solution

--- src/day_5/task_one.py
+++ src/day_5/task_one.py
@@ -1,16 +1,34 @@
 def main():
-    print("Hello World!")
+    printer = Printer()
+    print(printer.count_updates_that_pass_all_rules())
+
+class Printer:
+    rules: list[tuple[int, int]]
+    updates: list[list[int]]
+
+    def __init__(self):
+        self.rules = get_rule_input()
+        self.updates = get_update_input()
+
+    def update_passes_all_rules(self, update: list[int]) -> bool:
+        for rule in self.rules:
+            if update_does_not_pass_rule(rule, update):
+                return False
+        return True
+
+    def count_updates_that_pass_all_rules(self) -> int:
+        count = 0
+        for update in self.updates:
+            if self.update_passes_all_rules(update):
+                count += find_middle_element(update)
+        return count
+
 
 def get_rule_input() -> list[tuple[int, int]]:
     task_input = get_input()
     string_rules = task_input[:1176]
     list_of_numbers_in_rules = [rule.split('|') for rule in string_rules]
     return [(int(rule[0]), int(rule[1])) for rule in list_of_numbers_in_rules]
-
-
-def get_input() -> list[str]:
-    with open("/Users/chris.rossell/projects/AdventOfCode2024/AdventOfCode2024/src/day_5/input.txt") as file:
-        return file.readlines()
 
 
 def get_update_input() -> list[list[int]]:
@@ -22,6 +40,30 @@ def get_update_input() -> list[list[int]]:
         updates.append([int(update_element) for update_element in update])
     return updates
 
+
+def get_input() -> list[str]:
+    with open("/Users/chris.rossell/projects/AdventOfCode2024/AdventOfCode2024/src/day_5/input.txt") as file:
+        return file.readlines()
+
+def update_does_not_pass_rule(rule: tuple[int, int], update: list[int]) -> bool:
+    return not update_passes_rule(rule, update)
+
+def update_passes_rule(rule: tuple[int, int], update: list[int]) -> bool:
+    first_number, second_number = rule
+    if first_number not in update or second_number not in update:
+        return True
+    first_number_index = update.index(first_number)
+    second_number_index = update.index(second_number)
+    return first_number_index < second_number_index
+
+
+def find_middle_element(update: list[int]) -> int:
+    max_index = len(update) - 1
+    middle_index = max_index / 2
+    if max_index % 2 == 0:
+        return update[int(middle_index)]
+    else:
+        raise ValueError(f"Middle index could not be found: even number of elements in {update}")
 
 if __name__ == "__main__":
     main()
--- test/day_5/test_task_one.py
+++ test/day_5/test_task_one.py
@@ -1,4 +1,8 @@
-from day_5.task_one import get_rule_input, get_input, get_update_input
+from unittest.mock import patch
+
+import pytest
+
+from day_5.task_one import get_rule_input, get_input, get_update_input, update_passes_rule, find_middle_element, Printer
 
 
 def test_get_input():
@@ -23,3 +27,101 @@ def test_get_update_input():
     assert actual[0] == [52, 77, 83, 31, 94, 75, 34, 95, 29, 38, 82, 19, 41, 39, 27, 98, 84, 13, 55, 21, 66]
     assert actual [-1] == [98, 66, 29, 75, 15, 68, 27, 67, 31, 13, 63, 93, 89, 46, 52, 87, 95, 18, 32, 19, 44, 74, 43]
 
+
+def test_update_passes_rule_happy_path():
+    rule = (32, 75)
+    update = [32, 75, 18, 19]
+    expected = True
+    actual = update_passes_rule(rule, update)
+    assert actual == expected
+
+
+def test_update_passes_rule_elements_not_adjacent():
+    rule = (32, 75)
+    update = [32, 18, 19, 75]
+    expected = True
+    actual = update_passes_rule(rule, update)
+    assert actual == expected
+
+
+def test_update_passes_rule_element_not_present():
+    rule = (32, 85)
+    update = [32, 75, 18, 19]
+    expected = True
+    actual = update_passes_rule(rule, update)
+    assert actual == expected
+
+
+def test_update_passes_rule_element_present_twice():
+    rule = (32, 75)
+    update = [32, 75, 18, 75]
+    expected = True
+    actual = update_passes_rule(rule, update)
+    assert actual == expected
+
+
+def test_update_passes_rule_element_present_twice_once_before():
+    rule = (32, 75)
+    update = [75, 32, 75]
+    expected = False
+    actual = update_passes_rule(rule, update)
+    assert actual == expected
+
+def test_find_middle_element_throws_error_where_even_number_of_elements():
+    update = [32, 75, 18, 19, 13, 14, 16, 17]
+
+    with pytest.raises(ValueError) as e:
+        find_middle_element(update)
+        assert e.value == "Middle index could not be found: even number of elements in [32, 75, 18, 19]"
+
+def test_find_middle_element_happy_path():
+    update = [32, 75, 18, 19, 44]
+    expected = 18
+    actual = find_middle_element(update)
+    assert actual == expected
+
+def test_find_middle_element_happy_path_more_elements():
+    update = [32, 75, 18, 19, 44, 55, 66]
+    expected = 19
+    actual = find_middle_element(update)
+    assert actual == expected
+
+@pytest.fixture
+@patch('day_5.task_one.get_update_input')
+@patch('day_5.task_one.get_rule_input')
+def printer(mock_rules, mock_updates):
+    mock_rules.return_value = [(32, 75), (18, 19)]
+    mock_updates.return_value = [[32, 75, 50, 18, 19]]
+    return Printer()
+
+def test_printer_update_passes_all_rules_happy_path(printer):
+    update = [32, 75, 18, 19]
+    expected = True
+    actual = printer.update_passes_all_rules(update)
+    assert actual == expected
+
+
+def test_printer_update_passes_all_rules_unhappy_path(printer):
+    update = [32, 75, 18]
+    expected = False
+    actual = printer.update_passes_all_rules(update)
+    assert actual == expected
+
+
+def test_count_updates_happy_path(printer):
+    expected = 50
+    actual = printer.count_updates_that_pass_all_rules()
+    assert actual == expected
+
+
+@patch('day_5.task_one.get_update_input')
+@patch('day_5.task_one.get_rule_input')
+def test_count_updates_does_not_count_invalid(mock_rules,
+                                                                          mock_updates):
+    mock_rules.return_value = [(32, 75), (18, 19)]
+    mock_updates.return_value = [[32, 75, 50, 18, 19], [12, 13, 75, 32, 3]]
+    printer = Printer()
+
+    expected = 50
+    actual = printer.count_updates_that_pass_all_rules()
+    assert actual == expected
