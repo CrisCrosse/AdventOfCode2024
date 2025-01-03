9704cb8664c964371453d86a4e6618b5e7a50d78
Chris Rossell <chris.rossell@autotrader.co.uk>
2025-01-03 09:01:34 +0000

    day 5 task one add get update input

--- src/day_5/task_one.py
+++ src/day_5/task_one.py
@@ -2,10 +2,10 @@ def main():
     print("Hello World!")
 
 def get_rule_input() -> list[tuple[int, int]]:
-    input = get_input()
-    rules = input[:1176]
-    list_of_list_of_str_numbers = [rule.split('|') for rule in rules]
-    return [(int(rule[0]), int(rule[1])) for rule in list_of_list_of_str_numbers]
+    task_input = get_input()
+    string_rules = task_input[:1176]
+    list_of_numbers_in_rules = [rule.split('|') for rule in string_rules]
+    return [(int(rule[0]), int(rule[1])) for rule in list_of_numbers_in_rules]
 
 
 def get_input() -> list[str]:
@@ -13,8 +13,15 @@ def get_input() -> list[str]:
         return file.readlines()
 
 
-def get_update_input():
-    pass
+def get_update_input() -> list[list[int]]:
+    task_input = get_input()
+    string_updates = task_input[1177:]
+    split_updates = [update.split(',') for update in string_updates]
+    updates = []
+    for update in split_updates:
+        updates.append([int(update_element) for update_element in update])
+    return updates
+
 
 if __name__ == "__main__":
     main()
--- test/day_5/test_task_one.py
+++ test/day_5/test_task_one.py
@@ -1,4 +1,4 @@
-from day_5.task_one import get_rule_input, get_input
+from day_5.task_one import get_rule_input, get_input, get_update_input
 
 
 def test_get_input():
@@ -17,4 +17,9 @@ def test_get_rule_input():
     assert actual[-1] == (38, 62)
 
 def test_get_update_input():
-    pass
+    actual = get_update_input()
+    assert isinstance(actual, list)
+    assert len(actual) == 185
+    assert actual[0] == [52, 77, 83, 31, 94, 75, 34, 95, 29, 38, 82, 19, 41, 39, 27, 98, 84, 13, 55, 21, 66]
+    assert actual [-1] == [98, 66, 29, 75, 15, 68, 27, 67, 31, 13, 63, 93, 89, 46, 52, 87, 95, 18, 32, 19, 44, 74, 43]
+
